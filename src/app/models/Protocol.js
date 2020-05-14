96436b7797f76b86204472af5dcf817dd91ee371
Caio Almeida <caioalmeidanaweb@gmail.com>
2020-05-14 14:56:51 -0400

    Installing lib and modules to use in system

--- src/app/models/Protocol.js
+++ src/app/models/Protocol.js
@@ -1,8 +1,49 @@
-import Sequelize, { Model } from 'sequelize';
+import Sequelize, { Model, NOW } from 'sequelize';
 
 class Protocol extends Model {
     static init(sequelize) {
-        super.init({}, { sequelize });
+        super.init(
+            {
+                from: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                to: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                status: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                    defaultValue: 'G',
+                },
+                delivery_date: {
+                    type: Sequelize.DATE,
+                    allowNull: true,
+                },
+                departue_date: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    defaultValue: NOW(),
+                },
+                description: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+            },
+            { sequelize }
+        );
+
+        return this;
+    }
+
+    static associate(models) {
+        this.belongsTo(models.User, {
+            foreignKey: 'user_id',
+            as: 'creator',
+        });
+
+        this.belongsTo(models.File, { foreignKey: 'file_id', as: 'image' });
     }
 }
 
--- src/database/index.js
+++ src/database/index.js
@@ -1,17 +1,15 @@
+import mongoose from 'mongoose';
 import Sequelize from 'sequelize';
-// import mongoose from 'mongoose';
-
-import databaseConfig from '../config/database';
-
-import User from '../app/models/User';
 import Protocol from '../app/models/Protocol';
+import User from '../app/models/User';
+import databaseConfig from '../config/database';
 
 const models = [User, Protocol];
 
 class Database {
     constructor() {
         this.init();
-        // this.mongo();
+        this.mongo();
     }
 
     init() {
@@ -29,12 +27,12 @@ class Database {
             );
     }
 
-    //   mongo() {
-    //     this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
-    //       useNewUrlParser: true,
-    //       useFindAndModify: true,
-    //     });
-    //   }
+    mongo() {
+        this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useFindAndModify: true,
+        });
+    }
 }
 
 export default new Database();
